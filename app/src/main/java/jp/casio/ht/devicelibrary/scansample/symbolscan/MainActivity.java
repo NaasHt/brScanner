89c662112423e7a101cb5808f6f8a29f238836d8
Ari <osipova.anastasija@gmail.com>
2022-08-01 12:44:23 +0300

    01/08

---
+++ app/src/main/java/jp/casio/ht/devicelibrary/scansample/symbolscan/MainActivity.java
@@ -0,0 +1,31 @@
+package jp.casio.ht.devicelibrary.scansample.symbolscan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button changeActivityBTN = findViewById(R.id.btnInv);
+
+        changeActivityBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeActivity();}
+        });
+
+    }
+    private void ChangeActivity(){
+        Intent intent =new Intent (this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+
+}
