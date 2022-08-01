90c731d2d6e33bc09134b3403a883bf47eba3bb1
Ari <osipova.anastasija@gmail.com>
2022-08-01 12:44:23 +0300

    01/08

---
+++ app/src/main/java/jp/casio/ht/devicelibrary/scansample/symbolscan/MainActivity2.java
@@ -0,0 +1,114 @@
+package jp.casio.ht.devicelibrary.scansample.symbolscan;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import java.io.File;  // Import the File class
+import java.io.IOException;
+import jp.casio.ht.devicelibrary.ScannerLibrary;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private static TextView mTextView1;
+    private static ScannerLibrary mScanLib;
+    private static ScannerLibrary.ScanResult mScanResult;
+    private static ScanResultReceiver mScanResultReceiver;
+
+    private static ScannerLibrary getmScanLib() {
+        if (mScanLib == null) {
+            mScanLib = new ScannerLibrary();
+        }
+        return mScanLib;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //1. Init Scanner
+        mScanLib = new ScannerLibrary();
+        mScanResult = new ScannerLibrary.ScanResult();
+        mScanResultReceiver = new ScanResultReceiver();
+        getmScanLib().openScanner();
+
+        mTextView1 = (TextView) findViewById(R.id.textView1);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getmScanLib().setTriggerKeyTimeout(3000);
+        getmScanLib().setOutputType(ScannerLibrary.CONSTANT.OUTPUT.USER);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("device.scanner.EVENT");
+        filter.addAction("device.common.USERMSG");
+        registerReceiver(mScanResultReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mScanResultReceiver);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        //4. Deinit Scanner
+        getmScanLib().closeScanner();
+        mScanLib = null;
+        mScanResult = null;
+        super.onDestroy();
+    }
+
+
+    public static class ScanResultReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            if (mScanLib != null) {
+                //3. Read barcode
+                getmScanLib().getScanResult(mScanResult);
+                if (mScanResult.length > 0) {
+                    mTextView1.setText(new String(mScanResult.value));
+                } else {
+                    mTextView1.setText("");
+                }
+            }
+        }
+    }
+
+    public class CreateFile {
+        public void main(String[] args) {
+            try {
+                File myObj = new File("filename.txt");
+                if (myObj.createNewFile()) {
+                    System.out.println("File created: " + myObj.getName());
+                } else {
+                    System.out.println("File already exists.");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/jp/casio/ht/devicelibrary/scansample/symbolscan/ScanResultReceiver.java
@@ -0,0 +1,12 @@
+package jp.casio.ht.devicelibrary.scansample.symbolscan;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ScanResultReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
